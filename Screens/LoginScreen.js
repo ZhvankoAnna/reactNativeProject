39e61b3525964da7ecda4397445f731f56cf9b0b
Zhvanko Anna <zhvankoann@gmail.com>
2023-06-02 20:05:07 +0300

    Fix show password

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -6,6 +6,7 @@ import {
   ImageBackground,
   TouchableWithoutFeedback,
   Keyboard,
+  TouchableOpacity,
 } from "react-native";
 import { useState } from "react";
 import bgImage from "../assets/bgImage.png";
@@ -32,19 +33,20 @@ export default function LoginScreen() {
                   onChange={setEmail}
                   value={email}
                 />
-              </KeyboardAvoidingView>
-              <KeyboardAvoidingView
-                behavior={Platform.OS === "ios" ? "padding" : "height"}
-              >
+                <View>
                 <FormInput
                   style={styles.input}
                   placeholder="Пароль"
                   onChange={setPassword}
                   value={password}
                 />
+                <TouchableOpacity style={styles.hideBtn}>
+                  <Text style={styles.hideText}>Показати</Text>
+                </TouchableOpacity>
+                </View>
+                <SubmitButton title="Увійти" />
               </KeyboardAvoidingView>
             </View>
-            <SubmitButton title="Увійти" />
             <Text style={styles.link}>Немає акаунту? Зареєструватися</Text>
           </View>
         </View>
@@ -80,41 +82,13 @@ const styles = StyleSheet.create({
     marginBottom: 32,
     textAlign: "center",
   },
-  input: {
-    minWidth: 343,
-    height: 50,
-    backgroundColor: "#F6F6F6",
-    borderColor: "#E8E8E8",
-    borderStyle: "solid",
-    borderWidth: 1,
-    borderRadius: 8,
-    padding: 16,
-    marginBottom: 16,
-    fontFamily: "rb-reg",
-    fontSize: 16,
-  },
   hideBtn: {
     position: "absolute",
+    top: 14,
     right: 16,
-    bottom: 31,
   },
   hideText: {
     color: "#1B4371",
-    fontFamily: "rb-reg",
-    fontSize: 16,
-  },
-  btn: {
-    minWidth: "100%",
-    height: 51,
-    marginTop: 27,
-    paddingVertical: 16,
-    paddingHorizontal: 32,
-    borderRadius: 100,
-    backgroundColor: "#FF6C00",
-  },
-  btnText: {
-    color: "#fff",
-    textAlign: "center",
     fontFamily: "rb-reg",
     fontSize: 16,
   },
